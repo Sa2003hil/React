fc87c6b9984a8620e9d67f45a322616a01d63604
Sahil <Sahil2003@gmail.com>
2024-01-03 12:17:29 +0530

    Theme Switch Project using context API done

--- Practise/07ContextAPI/themeswitcher/src/App.jsx
+++ Practise/07ContextAPI/themeswitcher/src/App.jsx
@@ -1,23 +1,49 @@
 import './App.css'
+import Card from './components/Card'
+import ThemeBtn from './components/ThemeBtn'
+import { ThemeProvider } from './contexts/theme'
+import { useEffect, useState } from 'react'
 
 function App() {
 
+  const [themeMode, setThemeMode] = useState('light')
+
+  // The functionality of the lightTheme and darkTheme methods are not yet implemented in the ThemeProvider context, so we can make the functionality it in the compenent itself by defining the methods(names must be same as in the context)
+
+  const lightTheme = () => {
+    setThemeMode('light')
+  }
+
+  const darkTheme = () => {
+    setThemeMode('dark')
+  }
+
+
+  // acctual change in the theme
+
+  useEffect(() => {
+    document.querySelector('html').classList.remove('dark', 'light')
+    document.querySelector('html').classList.add(themeMode)
+  }, [themeMode])
+
 
   return (
-    <>
+    <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
       <div className=' text-lg font-semibold'>Welcome to the theme switcher</div>
       <div className="flex flex-wrap min-h-screen items-center">
         <div className="w-full">
           <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
             {/* Themebtn */}
+            <ThemeBtn />
           </div>
 
           <div className="w-full max-w-sm mx-auto">
             {/* Cards */}
+            <Card />
           </div>
         </div>
       </div>
-    </>
+    </ThemeProvider>
   )
 }
 
---
+++ Practise/07ContextAPI/themeswitcher/src/contexts/theme.js
@@ -0,0 +1,25 @@
+import { createContext, useContext } from 'react';
+
+/* 
+This is the different approach of making the context in one file only and not making a separate file for provider
+
+1. We are providing the default value of the context in the provider (value={{ themeMode & two methods---->  lightTheme, darkTheme }})
+
+
+*/
+export const ThemeContext = createContext({
+    themeMode: 'light',
+    // These are two methods which we will use to change the theme
+    lightTheme: () => { },
+    darkTheme: () => { },
+});
+
+
+// Here we are using provider in the same file and not making a separate file for it
+export const ThemeProvider = ThemeContext.Provider;
+
+
+// creating a custom hook (In the previous approach we have to import useContext and UserContext in every file where we want to use the context) so to avoid this we are making a custom hook
+export default function useTheme() {
+    return useContext(ThemeContext)
+}
